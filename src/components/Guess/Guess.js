6a32405888f44c494a930e76229e5513a46f688e
Patrick Karjala <pkarjala@gmail.com>
2025-08-07 11:18:20 -1000

    Exercise 4 completion state.

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,14 +1,28 @@
 import React from 'react';
 
 import { range } from '../../utils';
+import { checkGuess } from '../../game-helpers'
 
-function Guess({ word }) {
+// Creates a specific cell for the guess being made.
+function Cell({ letter, status }) {
+  // Veryfiy status has an actual value before setting it as the className.
+  const className = status ? `cell ${status}` : 'cell';
+  return (
+    <span className={className}>{letter}</span>
+  )
+}
+
+function Guess({ guessedWord, answer }) {
+  const result = checkGuess(guessedWord, answer);
+  
   return (
     <p className="guess">
       {range(5).map((index) => (
-        <span key={index} className="cell">
-          {word ? word[index] : undefined}
-        </span>
+        <Cell 
+          key={index} 
+          letter={result ? result[index].letter : undefined}
+          status={result ? result[index].status : undefined}
+        />
       ))}
     </p>
   );
