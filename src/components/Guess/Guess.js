8975103ddee5da5ce380cd8b67853b9d85aa27f9
Patrick Karjala <pkarjala@gmail.com>
2025-08-07 07:45:11 -1000

    Exercise 3 completion state.

---
+++ src/components/Guess/Guess.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { range } from '../../utils';
+
+function Guess({ word }) {
+  return (
+    <p className="guess">
+      {range(5).map((index) => (
+        <span key={index} className="cell">
+          {word ? word[index] : undefined}
+        </span>
+      ))}
+    </p>
+  );
+}
+
+export default Guess;
